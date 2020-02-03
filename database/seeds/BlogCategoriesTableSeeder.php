cc5080a5ee1d8acf53ad013bf83e297a5c0ba0dd
Jettio <contrakt120@gmail.com>
2020-02-03 14:00:09 +0300

    Auth, migrations, seeds

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName
+        ];
+
+        for ($i = 1; $i <= 10; $i++) {
+            $cName = 'Категория №'.$i;
+
+            $categories[] = [
+                'title' => $cName
+            ];
+        }
+
+        DB::table('blog_categories')->insert($categories);
+
+    }
+}
