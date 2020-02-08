e22126a73bbd292f2ed95cd035a177aa9da5cce0
Jettio <contrakt120@gmail.com>
2020-02-08 14:26:47 +0300

    fix one more error

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -21,7 +21,7 @@
 					<tr>
 						<td>{{$category->title}}</td>
 						<td>
-							<a href="{{ route ('admin.category.edit', ['id'=>$category->id]) }}"><i class="fa fa-edit"></i></a>
+							<a href="{{ route ('admin.category.edit', ['category'=>$category->id]) }}"><i class="fa fa-edit"></i></a>
 						</td>
 
 					</tr>
